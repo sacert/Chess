72d6c0ea9ad9aa5c31218f1749c84589d96e3aea
stephen <stephen@192.168.0.12>
2015-08-19 01:46:54 -0700

    small fixes -- indenting, moving useless testing code

--- ChessGame/src/board/Board.java
+++ ChessGame/src/board/Board.java
@@ -318,7 +318,6 @@ public class Board implements Constants {
 							mm = (Move) moves.elementAt(k);
 							//System.out.println(mm.type);
 							if((board[mm.y2][mm.x2] != null && board[mm.y2][mm.x2].type == 5) && board[mm.y2][mm.x2].isWhite == false) {
-								System.out.println(i + " " + j);
 								System.out.println("Black in check");
 								check = true;
 							}
@@ -433,67 +432,67 @@ public class Board implements Constants {
 	}
 		
 	// determine if the move will cause the player to be in check
-		// output options that will remove the player from being in check
-		public static boolean blackCheck(int x1, int y1, int x2, int y2, Piece[][] board) {
-			Main getTurn = new Main();
-			Vector moves = new Vector();
-			Move mm;
-			boolean check = false;
-			
-			Piece[][] boardCopy = new Piece[8][8]; // play the copy of the board in here
-			
-			// copy the board
-			for ( int k = 0; k < 8; k++) {
-				for ( int l = 0; l < 8; l++) {
-					if( board[k][l] != null) {
-						boardCopy[k][l] = board[k][l];
-					}
+	// output options that will remove the player from being in check
+	public static boolean blackCheck(int x1, int y1, int x2, int y2, Piece[][] board) {
+		Main getTurn = new Main();
+		Vector moves = new Vector();
+		Move mm;
+		boolean check = false;
+		
+		Piece[][] boardCopy = new Piece[8][8]; // play the copy of the board in here
+		
+		// copy the board
+		for ( int k = 0; k < 8; k++) {
+			for ( int l = 0; l < 8; l++) {
+				if( board[k][l] != null) {
+					boardCopy[k][l] = board[k][l];
 				}
 			}
-			
-			// within the copied board, imitate as if the move had been selected
-			boardCopy[x2][y2] = board[x1][y1];
-			boardCopy[x1][y1] = null;
-			
-			// check if the move will cause the player to be in check
-			if(getTurn.isWhiteTurn == false) {
-				for(int i = 0; i < 8; i++) {
-					for(int j = 0; j < 8; j++) {
-						if(boardCopy[i][j] != null && boardCopy[i][j].isWhite) {
-							switch(boardCopy[i][j].type){
-							case PAWN:
-								moves = PieceAlgorithms.pawn(boardCopy,i,j);
-								break;
-							case ROOK:
-								moves = PieceAlgorithms.rook(boardCopy,i,j);
-								break;
-							case BISHOP:
-								moves = PieceAlgorithms.bishop(boardCopy,i,j);
-								break;
-							case QUEEN:
-								moves = PieceAlgorithms.queen(boardCopy,i,j);
-								break;
-							//case KING:
-								//moves = PieceAlgorithms.king(boardCopy,i,j);
-								//break;
-							case KNIGHT:
-								moves = PieceAlgorithms.knight(boardCopy,i,j);
-								break;
-							}
-							
-							// check if the possible moves are a king
-							for( int k = 0; k < moves.size(); k++) {
-								mm = (Move) moves.elementAt(k);
-								if(boardCopy[mm.y2][mm.x2] != null && boardCopy[mm.y2][mm.x2].type == 5) {
-									check = true;
-								}
+		}
+		
+		// within the copied board, imitate as if the move had been selected
+		boardCopy[x2][y2] = board[x1][y1];
+		boardCopy[x1][y1] = null;
+		
+		// check if the move will cause the player to be in check
+		if(getTurn.isWhiteTurn == false) {
+			for(int i = 0; i < 8; i++) {
+				for(int j = 0; j < 8; j++) {
+					if(boardCopy[i][j] != null && boardCopy[i][j].isWhite) {
+						switch(boardCopy[i][j].type){
+						case PAWN:
+							moves = PieceAlgorithms.pawn(boardCopy,i,j);
+							break;
+						case ROOK:
+							moves = PieceAlgorithms.rook(boardCopy,i,j);
+							break;
+						case BISHOP:
+							moves = PieceAlgorithms.bishop(boardCopy,i,j);
+							break;
+						case QUEEN:
+							moves = PieceAlgorithms.queen(boardCopy,i,j);
+							break;
+						//case KING:
+							//moves = PieceAlgorithms.king(boardCopy,i,j);
+							//break;
+						case KNIGHT:
+							moves = PieceAlgorithms.knight(boardCopy,i,j);
+							break;
+						}
+						
+						// check if the possible moves are a king
+						for( int k = 0; k < moves.size(); k++) {
+							mm = (Move) moves.elementAt(k);
+							if(boardCopy[mm.y2][mm.x2] != null && boardCopy[mm.y2][mm.x2].type == 5) {
+								check = true;
 							}
 						}
 					}
 				}
 			}
-			
-			return check;
 		}
+		
+		return check;
+	}
 	
 }
