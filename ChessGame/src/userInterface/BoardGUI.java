05ce42f3df4a73b6f134a34aac85c27731376deb
stephen <stephen@192.168.0.10>
2015-08-25 03:45:34 -0700

    test

---
+++ ChessGame/src/userInterface/BoardGUI.java
@@ -0,0 +1,18 @@
+package userInterface;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BoardGUI extends Application{
+	
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
