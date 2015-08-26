4bebd47ac299b53ccaae62bf35e96118cdbf9fca
dtufail <dtufail@sfu.ca>
2015-08-26 15:51:23 -0700

    The user can click away now to select a new piece.

--- ChessGameJavaFX/src/application/FXController.java
+++ ChessGameJavaFX/src/application/FXController.java
@@ -197,7 +197,8 @@ public class FXController implements Initializable, Constants {
 						refreshBoard(board);
 						printWhoseTurn();
 					} else {
-						selectingPiece = false;
+						selectingPiece = true;
+						refreshBoard(board);
 						printWhoseTurn();
 					}
 				}
